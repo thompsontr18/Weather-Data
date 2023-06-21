4653c555f89dd6ca88719c2e16deb537ca3952bf
Trevor Thompson <132678966+thompsontr18@users.noreply.github.com>
2023-06-21 10:25:50 -0400

    Update weather.py

--- weather.py
+++ weather.py
@@ -17,7 +17,7 @@ city = input("> Enter your city: ")
 url = "https://www.wunderground.com/weather/us/"+state+"/"+city
 
 
-DICT = pathlib.Path(__file__).parent / "𝙛𝙞𝙡𝙚𝙣𝙖𝙢𝙚"
+DICT = pathlib.Path(__file__).parent / "𝙛𝙞𝙡𝙚𝙣𝙖𝙢𝙚.𝙩𝙭𝙩"
 
 cities = {
 	word.title()
